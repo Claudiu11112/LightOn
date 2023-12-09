16cbf02696c9813833d0af664ca7e4da51ff143b
Popjo <mhvsl@yahoo.com>
2023-12-09 12:51:19 +0100

    12:46 PM 12/9/2023

--- src/test/LightOn.java
+++ src/test/LightOn.java
@@ -16,7 +16,7 @@ public class LightOn {
                 LightOn window = new LightOn();
                 window.f.setVisible(true);
             } catch (Exception e) {
-                e.printStackTrace();
+//                e.printStackTrace();
             }
         });
     }
@@ -59,51 +59,10 @@ public class LightOn {
         lbl3.setBounds(176, 74, 70, 42);
         f.getContentPane().add(lbl3);
 
-        JButton btnOn = new JButton("ON");
-        btnOn.addActionListener(e -> {
-            lbl3.setText("ON");
-            lbl3.setForeground(Color.GREEN);
-
-            Thread t = new Thread(() -> {
-                URL url1;
-                try {
-                    String a = "http://192.168.1.111?pin=ON";
-                    url1 = new URL(a);
-                    URLConnection conn = url1.openConnection();
-                    InputStream in = conn.getInputStream();
-                    in.close();
-                } catch (IOException e1) {
-                    //joie1.printStackTrace();
-                }
-            });
-            t.start();
-        });
-        btnOn.setBackground(new Color(230, 230, 250));
-        btnOn.setFont(new Font("Dialog", Font.PLAIN, 20));
-        btnOn.setBounds(209, 117, 79, 34);
+        JButton btnOn = getBtnOn(lbl3);
         f.getContentPane().add(btnOn);
 
-        JButton btnOff = new JButton("OFF");
-        btnOff.addActionListener(e -> {
-            lbl3.setText("OFF");
-            lbl3.setForeground(Color.MAGENTA);
-            Thread t = new Thread(() -> {
-                URL url1;
-                try {
-                    String a = "http://192.168.1.111?pin=OFF";
-                    url1 = new URL(a);
-                    URLConnection conn = url1.openConnection();
-                    InputStream in = conn.getInputStream();
-                    in.close();
-                } catch (IOException e1) {
-                   // e1.printStackTrace();
-                }
-            });
-            t.start();
-        });
-        btnOff.setFont(new Font("Dialog", Font.PLAIN, 20));
-        btnOff.setBackground(new Color(230, 230, 250));
-        btnOff.setBounds(129, 117, 79, 34);
+        JButton btnOff = getBtnOff(lbl3);
         f.getContentPane().add(btnOff);
 
         JLabel lbl4 = new JLabel("OFF");
@@ -111,29 +70,24 @@ public class LightOn {
         lbl4.setBounds(383, 74, 70, 42);
         f.getContentPane().add(lbl4);
 
-        JButton btnOff1 = new JButton("OFF");
-        btnOff1.addActionListener(e -> {
-            lbl4.setText("OFF");
-            lbl4.setForeground(Color.MAGENTA);
-            Thread t = new Thread(() -> {
-                URL url1;
-                try {
-                    String a = "http://192.168.1.111?pin1=OFF";
-                    url1 = new URL(a);
-                    URLConnection conn = url1.openConnection();
-                    InputStream in = conn.getInputStream();
-                    in.close();
-                } catch (IOException e1) {
-                  //  e1.printStackTrace();
-                }
-            });
-            t.start();
-        });
-        btnOff1.setFont(new Font("Dialog", Font.PLAIN, 20));
-        btnOff1.setBackground(new Color(230, 230, 250));
-        btnOff1.setBounds(336, 117, 79, 34);
+        JButton btnOff1 = getBtnOff1(lbl4);
         f.getContentPane().add(btnOff1);
 
+        JButton btnOn1 = getBtnOn1(lbl4);
+        f.getContentPane().add(btnOn1);
+
+        JLabel lbl5 = new JLabel("Light switch");
+        lbl5.setFont(new Font("Dialog", Font.BOLD, 16));
+        lbl5.setBounds(360, 45, 138, 19);
+        f.getContentPane().add(lbl5);
+
+        JLabel lbl6 = new JLabel();
+        lbl6.setBounds(12, 12, 107, 104);
+        f.getContentPane().add(lbl6);
+        lbl6.setIcon(i);
+    }
+
+    private static JButton getBtnOn1(JLabel lbl4) {
         JButton btnOn1 = new JButton("ON");
         btnOn1.addActionListener(e -> {
             lbl4.setText("ON");
@@ -155,16 +109,82 @@ public class LightOn {
         btnOn1.setFont(new Font("Dialog", Font.PLAIN, 20));
         btnOn1.setBackground(new Color(230, 230, 250));
         btnOn1.setBounds(416, 117, 79, 34);
-        f.getContentPane().add(btnOn1);
+        return btnOn1;
+    }
 
-        JLabel lbl5 = new JLabel("Light switch");
-        lbl5.setFont(new Font("Dialog", Font.BOLD, 16));
-        lbl5.setBounds(360, 45, 138, 19);
-        f.getContentPane().add(lbl5);
+    private static JButton getBtnOff1(JLabel lbl4) {
+        JButton btnOff1 = new JButton("OFF");
+        btnOff1.addActionListener(e -> {
+            lbl4.setText("OFF");
+            lbl4.setForeground(Color.MAGENTA);
+            Thread t = new Thread(() -> {
+                URL url1;
+                try {
+                    String a = "http://192.168.1.111?pin1=OFF";
+                    url1 = new URL(a);
+                    URLConnection conn = url1.openConnection();
+                    InputStream in = conn.getInputStream();
+                    in.close();
+                } catch (IOException e1) {
+                  //  e1.printStackTrace();
+                }
+            });
+            t.start();
+        });
+        btnOff1.setFont(new Font("Dialog", Font.PLAIN, 20));
+        btnOff1.setBackground(new Color(230, 230, 250));
+        btnOff1.setBounds(336, 117, 79, 34);
+        return btnOff1;
+    }
 
-        JLabel lbl6 = new JLabel();
-        lbl6.setBounds(12, 12, 107, 104);
-        f.getContentPane().add(lbl6);
-        lbl6.setIcon(i);
+    private static JButton getBtnOff(JLabel lbl3) {
+        JButton btnOff = new JButton("OFF");
+        btnOff.addActionListener(e -> {
+            lbl3.setText("OFF");
+            lbl3.setForeground(Color.MAGENTA);
+            Thread t = new Thread(() -> {
+                URL url1;
+                try {
+                    String a = "http://192.168.1.111?pin=OFF";
+                    url1 = new URL(a);
+                    URLConnection conn = url1.openConnection();
+                    InputStream in = conn.getInputStream();
+                    in.close();
+                } catch (IOException e1) {
+                   // e1.printStackTrace();
+                }
+            });
+            t.start();
+        });
+        btnOff.setFont(new Font("Dialog", Font.PLAIN, 20));
+        btnOff.setBackground(new Color(230, 230, 250));
+        btnOff.setBounds(129, 117, 79, 34);
+        return btnOff;
+    }
+
+    private static JButton getBtnOn(JLabel lbl3) {
+        JButton btnOn = new JButton("ON");
+        btnOn.addActionListener(e -> {
+            lbl3.setText("ON");
+            lbl3.setForeground(Color.GREEN);
+
+            Thread t = new Thread(() -> {
+                URL url1;
+                try {
+                    String a = "http://192.168.1.111?pin=ON";
+                    url1 = new URL(a);
+                    URLConnection conn = url1.openConnection();
+                    InputStream in = conn.getInputStream();
+                    in.close();
+                } catch (IOException e1) {
+                    //e1.printStackTrace();
+                }
+            });
+            t.start();
+        });
+        btnOn.setBackground(new Color(230, 230, 250));
+        btnOn.setFont(new Font("Dialog", Font.PLAIN, 20));
+        btnOn.setBounds(209, 117, 79, 34);
+        return btnOn;
     }
 }
